317c435d11a9d80f83507d2267e2d41f41429311
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-07-12 15:02:05 +0700

    [Fix] add middleware to protect customer api

--- components/customer/customer-api.js
+++ components/customer/customer-api.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
+const { authMiddleware } = require('../../middlewares/auth-middleware')
 const customerController = require('./customer-controller')
 
-router.post('/customer/1', customerController.addCustomerDataPart1)
-router.post('/customer/2', customerController.addCustomerDataPart2)
+router.post('/customer/1', authMiddleware, customerController.addCustomerDataPart1)
+router.post('/customer/2', authMiddleware, customerController.addCustomerDataPart2)
 
 module.exports = router
