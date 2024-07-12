31befd55b3da449c6aeaafb2578fab9e949ea226
Austin Gabriel Pardosi <gabrielpardosi26@gmail.com>
2024-07-13 00:22:34 +0700

    [Fix] recommendation

--- components/recommendation/recommendation-algorithm.js
+++ components/recommendation/recommendation-algorithm.js
@@ -1,6 +1,6 @@
 module.exports = {
     checkSimilarities(arr1, arr2) {
-        const similarities = 0;
+        let similarities = 0;
 
         arr1.forEach(item => {
             if (arr2.includes(item)) {
