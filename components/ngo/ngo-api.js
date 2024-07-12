e163c6743e19e24a562eeb60bc79804a6d6baf6c
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-07-12 18:14:11 +0700

    [Refactor] change route name

--- components/ngo/ngo-api.js
+++ components/ngo/ngo-api.js
@@ -4,10 +4,10 @@ const authMiddleware = require("../../middlewares/auth-middleware");
 
 const router = express.Router();
 
-router.post("/NGO", authMiddleware.authMiddleware, ngoController.createNGO);
-router.get("/getNGO/:id", authMiddleware.authMiddleware, ngoController.getNGO);
-router.get("/getAllNGO", authMiddleware.authMiddleware, ngoController.getAllNGOs);
-router.put("/NGO/:id", authMiddleware.authMiddleware, ngoController.updateNGO);
-router.delete("/NGO/:id", authMiddleware.authMiddleware, ngoController.deleteNGO);
+router.post("/ngo", authMiddleware.authMiddleware, ngoController.createNGO);
+router.get("/ngo/:id", authMiddleware.authMiddleware, ngoController.getNGO);
+router.get("/ngo/all", authMiddleware.authMiddleware, ngoController.getAllNGOs);
+router.put("/ngo/:id", authMiddleware.authMiddleware, ngoController.updateNGO);
+router.delete("/ngo/:id", authMiddleware.authMiddleware, ngoController.deleteNGO);
 
 module.exports = router;
